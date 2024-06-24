df4245ee198e0f8d0347fe50354116354136dc78
Matheus <matheustrabalhoo314@gmail.com>
2024-06-24 11:52:12 -0300

    Atualizando Listas

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -1,14 +1,19 @@
+# Declarando uma lista com três elementos str
 frutas = ["laranja", "maca", "uva"]
 print(frutas)
 
+# Declarando uma lista vazia
 frutas = []
 print(frutas)
 
+# Declarando com construtor de list - O argumento deve ser um elemento iteravél
 letras = list("python")
 print(letras)
 
+# Declarando com construtor de list - O argumento deve ser um elemento iteravél
 numeros = list(range(10))
 print(numeros)
 
+# Declarando uma lista de atributos de um carro
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
--- 01 - Estrutura de dados/01 - Listas/09_copy.py
+++ 01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -1,5 +1,12 @@
 lista = [1, "Python", [40, 30, 20]]
 
-lista.copy()
+l2 = lista.copy()
 
 print(lista)  # [1, "Python", [40, 30, 20]]
+
+print(id(lista), id(l2))
+
+l2[0] = 2
+
+print(lista)
+print(l2)
